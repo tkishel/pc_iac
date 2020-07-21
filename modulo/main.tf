011d67df7bd645162f45d2ed05c27b0aa9e53466
tkishel <tkishel@gmail.com>
2020-07-21 16:30:12 -0700

    Testing Prisma Cloud for GitHub

--- modulo/main.tf
+++ modulo/main.tf
@@ -1,8 +1,11 @@
 resource "aws_s3_bucket" "foo" {
   bucket        = "bar"
-  acl           = "public-read-write"
-  force_destroy = false
+  acl           = "private"
+  force_destroy = true
   versioning {
      enabled = false
   }  
 }
+
+#   acl           = "public-read-write"
+#   acl           = "private"
