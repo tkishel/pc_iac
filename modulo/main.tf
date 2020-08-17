b3f9bdb4faf4e99b489a588cb130475ab9fd1236
tkishel <tkishel@gmail.com>
2020-08-17 14:17:25 -0700

    better workaround v11

--- modulo/main.tf
+++ modulo/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "${var.region}"
 }
 
 resource "aws_s3_bucket" "foo" {
   bucket        = "bar-pc-iac"
-  region        = "us-west-2"
+  region        = "${var.region}"
   acl           = "public-read-write"
   force_destroy = true
   versioning {
@@ -12,12 +12,11 @@ resource "aws_s3_bucket" "foo" {
   }
 }
 
-# 
-
 # For comparison:
 #
 # acl: "public-read-write" vs "private"
 # versioning enabled: true vs false
+
 
 # We don't have support for Azure or Google and Terraform 0.12 yet.
 #
@@ -25,25 +24,15 @@ resource "aws_s3_bucket" "foo" {
 # but the plugins cannot scan a plan file,
 # or Azure or Google Terraform 0.12 templates.
 
-#provider "google" {
-#  region      = "us-west1"
-#}
-#
-#resource "google_storage_bucket" "foo" {
-#  name          = "bar-pc-iac"
-#  location      = "us-west1"
-#  force_destroy = true
-#}
-
 #provider "azurerm" {
 #  features {}
 #}
-
+#
 #resource "azurerm_resource_group" "foo" {
 #  name     = "bar"
 #  location = "West Central US"
 #}
-
+#
 #resource "azurerm_storage_account" "foo" {
 #  name                     = "bar"
 #  resource_group_name      = azurerm_resource_group.foo.name
@@ -53,4 +42,14 @@ resource "aws_s3_bucket" "foo" {
 #  network_rules {
 #    default_action = "Allow"
 #  }
+#}
+
+#provider "google" {
+#  region      = "us-west1"
+#}
+#
+#resource "google_storage_bucket" "foo" {
+#  name          = "bar-pc-iac"
+#  location      = "us-west1"
+#  force_destroy = true
 #}
